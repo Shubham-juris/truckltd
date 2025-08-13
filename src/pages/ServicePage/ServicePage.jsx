37bb70f45cfb01dbe8d72007037fb9003659856a
Vinay730 <vinay.juris@gmail.com>
2025-08-13 15:02:50 +0100

    add page

--- src/pages/ServicePage/ServicePage.jsx
+++ src/pages/ServicePage/ServicePage.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Services from '../../components/Servicecomponent/Services'
 import ServiceHero from '../../components/Servicecomponent/ServiceHero'
 import StatsSection from '../../components/Homecomponent/StateSection'
+import Servicecontent from '../../components/Homecomponent/Servicecontent'
 
 const ServicePage = () => {
   return (
     <>
     <ServiceHero/>
+    <Servicecontent />
      <Services/> 
      <StatsSection/>
     </>
