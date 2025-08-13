dc1c5bec7d2fa3568124162afaead7575d4bf998
Vinay730 <vinay.juris@gmail.com>
2025-08-13 14:52:59 +0100

    changes

--- src/components/Homecomponent/ChooseUs.jsx
+++ src/components/Homecomponent/ChooseUs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import img from "../../assets/Home/choose.webp";
+import img from "../../assets/Home/truck1.avif";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
